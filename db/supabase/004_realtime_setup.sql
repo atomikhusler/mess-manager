65db61d91314ec48362fdf202292b958e19f424b
SAHIL KUMAR ROUT <sahilkumarrout01@gmail.com>
2026-09-21 09:11:16 +0530

    Delete db/supabase directory

--- db/supabase/004_realtime_setup.sql
+++
@@ -1,9 +0,0 @@
--- Enable the Supabase Realtime publication
-BEGIN;
-  DROP PUBLICATION IF EXISTS supabase_realtime;
-  CREATE PUBLICATION supabase_realtime;
-COMMIT;
-
--- Attach our critical tables to the WebSocket engine for 0-latency sync
-ALTER PUBLICATION supabase_realtime ADD TABLE messes;
-ALTER PUBLICATION supabase_realtime ADD TABLE announcements;
